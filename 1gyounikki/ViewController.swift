b6b7cedd117ebc660ba2dce8783738d6a92f60d6
YOSHI091993 <krs0969@gmail.com>
2020-07-24 19:17:14 +0900

    Initial Commit

---
+++ 1gyounikki/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  1gyounikki
+//
+//  Created by 吉和　匠 on 2020/07/24.
+//  Copyright © 2020 SHO Yoshiwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
